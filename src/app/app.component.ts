ba376594a7ce4ac7e5b069e75d0049a6dc452da0
Audrey Rozowykwiat <audrey@mac-004797.ad.univ-paris3.fr>
2019-03-12 12:04:21 +0100

    Sauv 2 : création d'un componant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: '{{title}}',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-cours2';
+  title = 'Ceci est le cours angular-cours2';
 }
