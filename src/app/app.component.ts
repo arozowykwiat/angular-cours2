e2ef6d59cee8b4b19918f30b58cfc8098ae8af70
Audrey Rozowykwiat <audrey@mac-004797.ad.univ-paris3.fr>
2019-03-13 17:51:17 +0100

    Sauv 3 : création d'une liste et ajout d'un élément caché

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
-  template: '{{title}}',
+  template: `<h1>{{title}}</h1>
+  			<div [hidden]='todos.length'>Aucun élément</div>
+  			<ul>
+  				<li *ngFor='let todo of todos; let i = index;'>
+  				({{i + 1}}) {{todo}}
+  				</li>
+  			</ul>
+  			<form (submit)='createTodo()'>
+				<input type='text' name='todoLabel'  [(ngModel)]='newTodo'/>
+  			</form>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Ceci est le cours angular-cours2';
+  title = "Ma Todo List";
+  todos : string[] =  [];
+  newTodo : string ='';
+  createTodo() {
+  	if (this.newTodo){
+  		this.todos.push(this.newTodo);
+  		this.newTodo='';
+  	}
+  }
 }
